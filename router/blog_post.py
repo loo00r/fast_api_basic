247537a3cee6bf729fac7ef54846a83102ec7c33
loo00r <sannymasters@gmail.com>
2025-02-15 16:01:03 +0200

    Initial commit

---
+++ router/blog_post.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+    
+
+@router.post('/new')
+def create_blog(blog: BlogModel):
+
+    return {'data': blog}
